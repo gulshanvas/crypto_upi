8380ef68f3ee0f924facaf158ebcb11f6aca8715
Gulshan Vasnani <gulshanslvasnani@gmail.com>
2024-12-07 14:04:01 +0530

    added blockchain js to manage sending of tx on chain

---
+++ src/blockchain.js
@@ -0,0 +1,43 @@
+// import { createPublicClient, createWalletClient, encodeFunctionData, http, parseEther } from 'viem'
+
+const { privateKeyToAccount  }= require('viem/accounts')
+const { odysseyTestnet } = require('viem/chains')
+
+const { createWalletClient, http, createPublicClient } = require('viem')
+
+const { eip7702Actions } = require('viem/experimental')
+
+const DELEGATOR_PRIVATE_KEY = process.env.DELEGATOR_PRIVATE_KEY
+
+const delegatorAccount = privateKeyToAccount(DELEGATOR_PRIVATE_KEY)
+
+const walletClient = createWalletClient({
+  account: delegatorAccount,
+  chain: odysseyTestnet,
+  transport: http("https://odyssey.ithaca.xyz")
+}).extend(eip7702Actions())
+
+const publicClient = createPublicClient({
+  account: delegatorAccount,
+  chain: odysseyTestnet,
+  transport: http("https://odyssey.ithaca.xyz")
+}).extend(eip7702Actions())
+
+async function SendSignedRawTransaction(signedTx) {
+
+  const rawTxHash = await walletClient.sendRawTransaction({ serializedTransaction: signedTx })
+
+  const receiptTx = await publicClient.waitForTransactionReceipt({ hash: rawTxHash })
+
+  console.log('receiptTx ', receiptTx)
+
+  return receiptTx;
+}
+
+module.exports = {
+  SendSignedRawTransaction
+}
+
+
+
+
